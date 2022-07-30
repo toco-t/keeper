0ecd0f2578e3c8f79d9ac670cb911dd1124c907f
toco <ttachibana@my.bcit.ca>
2022-07-30 12:42:31 -0700

    Create a React app in index.js

--- src/index.js
+++ src/index.js
@@ -10,3 +10,10 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
