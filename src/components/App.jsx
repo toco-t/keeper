e51696ad9555775976026aa472227d6d7f7368be
toco <ttachibana@my.bcit.ca>
2022-07-31 17:13:52 -0700

    Refactor App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,13 @@ import Note from "./Note";
 import Footer from "./Footer";
 import notes from "../notes";
 
-function populateNotes(note) {
-	return <Note key={note.key} title={note.title} content={note.content} />;
-}
-
 function App() {
 	return (
 		<div>
 			<Header />
-			{notes.map(populateNotes)}
+			{notes.map((note) => (
+				<Note key={note.key} title={note.title} content={note.content} />
+			))}
 			<Footer />
 		</div>
 	);
