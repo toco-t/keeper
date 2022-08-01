d9fdbe42a37f48c68393fe974c4f5132dd286347
toco <ttachibana@my.bcit.ca>
2022-07-31 17:09:37 -0700

    Populate notes dynamically using map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
+
+function populateNotes(note) {
+	return <Note key={note.key} title={note.title} content={note.content} />;
+}
 
 function App() {
 	return (
 		<div>
 			<Header />
-			<Note />
+			{notes.map(populateNotes)}
 			<Footer />
 		</div>
 	);
