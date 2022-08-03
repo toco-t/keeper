6f83a3738f5d3da6690cb484e2b1c44fa606f72e
toco <ttachibana@my.bcit.ca>
2022-08-02 19:21:18 -0700

    Add material icons and background image

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
 	const [todo, setTodo] = useState({
 		title: "",
 		content: "",
 	});
+	const [isFocused, setFocused] = useState(false);
 
 	function reflectChanges(event) {
 		const { value, name } = event.target;
@@ -18,29 +22,37 @@ function CreateArea(props) {
 
 	return (
 		<div>
-			<form>
-				<input
-					onChange={reflectChanges}
-					name="title"
-					placeholder="Title"
-					value={todo.title}
-				/>
+			<form className="create-note">
+				{isFocused && (
+					<input
+						onChange={reflectChanges}
+						name="title"
+						placeholder="Title"
+						value={todo.title}
+					/>
+				)}
 				<textarea
 					onChange={reflectChanges}
+					onMouseOver={() => {
+						setFocused(true);
+					}}
 					name="content"
 					placeholder="Take a note..."
-					rows="3"
+					rows={isFocused ? "3" : "1"}
+					value={todo.content}
 				/>
-				<button
-					onClick={() => {
-						props.add(todo);
-						setTodo({ title: "", content: "" });
-						document.querySelector("textarea").value = "";
-					}}
-					type="button"
-				>
-					Add
-				</button>
+				<Zoom in={isFocused}>
+					<Fab
+						onClick={() => {
+							props.add(todo);
+							setTodo({ title: "", content: "" });
+							setFocused(false);
+						}}
+						type="button"
+					>
+						<AddIcon style={{ verticalAlign: "middle" }} />
+					</Fab>
+				</Zoom>
 			</form>
 		</div>
 	);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import HightlightIcon from "@material-ui/icons/Highlight";
 
 function Header() {
 	return (
 		<header>
-			<h1>Keeper</h1>
+			<h1>
+				<HightlightIcon />
+				Keeper
+			</h1>
 		</header>
 	);
 }
